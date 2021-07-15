2b5f24ac744907f702b7c5356c318f2055325a10
june0122 <june0122@naver.com>
2021-07-15 16:08:45 +0900

    feat: 이중 스택 기반 큐 구현 추가

--- src/queue/doublestack/StackQueue.kt
+++ src/queue/doublestack/StackQueue.kt
@@ -3,7 +3,44 @@ package queue.doublestack
 import base.Queue
 import stack.StackImpl
 
-//class StackQueue<T : Any> : Queue<T> {
-//    private val leftStack = StackImpl<T>()
-//    private val rightStack = StackImpl<T>()
-//}
+class StackQueue<T : Any> : Queue<T> {
+    private val leftStack = StackImpl<T>()
+    private val rightStack = StackImpl<T>()
+
+    override val count: Int
+        get() = leftStack.count + rightStack.count
+
+    override val isEmpty: Boolean
+        get() = leftStack.isEmpty && rightStack.isEmpty
+
+    private fun transferElements() {
+        var nextElement = rightStack.pop()
+        while (nextElement != null) {
+            leftStack.push(nextElement)
+            nextElement = rightStack.pop()
+        }
+    }
+
+    override fun peek(): T? {
+        if (leftStack.isEmpty) {
+            transferElements()
+        }
+        return leftStack.peek()
+    }
+
+    override fun enqueue(element: T): Boolean {
+        rightStack.push(element)
+        return true
+    }
+
+    override fun dequeue(): T? {
+        if (leftStack.isEmpty) {
+            transferElements()
+        }
+        return leftStack.pop()
+    }
+
+    override fun toString(): String {
+        return "Left stack: \n$leftStack \nRight stack: \n$rightStack"
+    }
+}
